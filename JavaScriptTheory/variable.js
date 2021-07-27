b3701c64a2997e1afaea1ee2d0769ff0054b4673
Himanshu Malik <jsm.28415@gmail.com>
2021-07-27 19:53:50 +0530

    Variable learn

---
+++ JavaScriptTheory/variable.js
@@ -0,0 +1,46 @@
+// JavaScript is loosely typed language, any variable can have data type;
+
+// let a = "abc";
+// let b = 'def';
+// let c = true;
+// let d = 12;
+// let e = 12.124;
+
+// console.log(a,b,c,d,e); // will print with spaces
+// console.log(a); // print in nextLine
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+// redefine and reassignation in different variable types;
+
+// let case -----------------
+//redefination not allowed
+// let z = 10;
+// let z = 20;
+// console.log(z);
+//reassigned allowed
+// let y = 10;
+// y = 20;
+// console.log(y);
+
+// var case ----------
+//redefination allowed
+// var g = 30;
+// var g = 40;
+// console.log(g);
+//reassigned allowed
+// var g = 30;
+// g = 40;
+// console.log(g);
+
+//const case ---------------
+//redefination not allowed
+// const a = 10;
+// const a = 20;
+// console.log(a);
+//reassigned not allowed
+// const a = 10;
+// a = 20;
+// console.log(a);
