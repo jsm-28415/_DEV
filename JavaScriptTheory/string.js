f520d8196733d318be267aab78f178f5f46e3c5c
Himanshu Malik <jsm.28415@gmail.com>
2021-07-27 20:08:31 +0530

    string learn

---
+++ JavaScriptTheory/string.js
@@ -0,0 +1,7 @@
+let firstName = `Himanshu`;
+let lastName = "Malik";
+console.log(firstName + " " + lastName);
+
+// this is called backtick, easy way to use.
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
