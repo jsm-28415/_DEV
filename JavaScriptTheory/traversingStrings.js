4355eca0550b848e1c72f8fff18fb133c27c5888
Himanshu Malik <jsm.28415@gmail.com>
2021-07-28 18:27:23 +0530

    travesring String

---
+++ JavaScriptTheory/traversingStrings.js
@@ -0,0 +1,12 @@
+// Printing it through loop
+// let str = "Bhavesh Bansal";
+// 
+// for(let i = 0; i < str.length; i++){
+    // console.log(str[i]);
+// }
+
+// Print character ASCII value
+let str = "Himanshu Malik";
+for(let i = 0; i < str.length; i++){
+    console.log(str.charCodeAt(i));
+}
