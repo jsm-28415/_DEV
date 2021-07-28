d745a5f5ab7a741fe361c38726d68d10dd1dc89f
Himanshu Malik <jsm.28415@gmail.com>
2021-07-28 20:39:15 +0530

    Arrays started

--- JavaScriptTheory/array.js
+++ JavaScriptTheory/array.js
@@ -1,13 +1,32 @@
-// Array creation
+// Array creation -> In JS array is dynamic
 
 // let arr = [];
 // let arr = new Array();
 // console.log(arr);
 
-let arr = ["bhavesh", 23, 45.4, true, 'b'];
+// let arr = ["bhavesh", 23, 45.4, true, 'b'];
+// 
+// arr.push("hello");
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// console.log(arr.length);
 
-arr.push("hello");
-console.log(arr);
-arr.pop();
-console.log(arr);
-console.log(arr.length);
+// let str1 = "Bhavesh";
+// let str2 = "Bhavesh Bansal";
+// let str3 = "apple, mango, banana";
+// split convert string into array; 
+// console.log(str1.split(""));
+// console.log(str2.split(""));
+// console.log(str3.split(","));
+
+// let arr = ["a", "b", "c", "d"];
+// join covert array into string;
+// console.log(arr.join(""));
+// console.log(arr.join("#"));
+// console.log(arr.join("|"));
+
+let arr1 = ["a", "b", "c", "d"];
+let arr2 = ["e", "f", "g", "h"];
+
+console.log(arr1.concat(arr2));
