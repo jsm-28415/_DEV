aa747bbe52e80b41991647eae1e5b8d6e05ff159
Himanshu Malik <jsm.28415@gmail.com>
2021-08-02 18:43:59 +0530

    Arrays_Not_finishYET

--- JavaScriptTheory/array.js
+++ JavaScriptTheory/array.js
@@ -26,7 +26,31 @@
 // console.log(arr.join("#"));
 // console.log(arr.join("|"));
 
-let arr1 = ["a", "b", "c", "d"];
-let arr2 = ["e", "f", "g", "h"];
+// let arr1 = ["a", "b", "c", "d"];
+// let arr2 = ["e", "f", "g", "h"];
+// 
+// console.log(arr1.concat(arr2));
 
-console.log(arr1.concat(arr2));
+//Normal way to print
+// let arr = [1,2,3,4,5,6];
+// for(let i = 0; i < arr.length; i++){
+    // console.log(arr[i]);
+// }
+
+//print i -> in = traverse and gives indexes
+// let arr = [8,9,7,6,5];
+// for(let i in arr){
+    // console.log(i);
+// }
+
+// print i -> of = traverse and gives value
+// let arr = [8,9,7,6,5];
+// for(let i of arr){
+    // console.log(i);
+// }
+
+// console.log("a" + 1 + 2); // String concatenatiom
+
+// console.log(1 + 2 + "a"); // first integer addition then string concatenation
+
+console.log(String.fromCharCode("a".charCodeAt(0) + 1));
