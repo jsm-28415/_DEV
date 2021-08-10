3ddd159b951451ccc4bb5f96d3be995d62a32c56
Himanshu Malik <jsm.28415@gmail.com>
2021-08-10 18:24:35 +0530

    Working with objects

--- JavaScriptTheory/object.js
+++ JavaScriptTheory/object.js
@@ -10,9 +10,27 @@
 // console.log(Array.prototype);
 // console.log(Array.prototype.map);
 
-String.prototype.len = function(){
-    console.log(this.length);
+// String.prototype.len = function(){
+    // console.log(this.length);
+// }
+// 
+// let name = "Bhavesh bansal";
+// name.len();
+
+let obj = {
+    1 : "hello1",
+    2 : "hello2"
 }
 
-let name = "Bhavesh bansal";
-name.len();
+// console.log(Object.keys(obj));
+// console.log(Object.values(obj));
+
+// Ways to iterate
+
+for(let key in obj){
+    console.log(obj[key]);
+}
+
+for(let i = 0; i < Object.keys(obj); i++){
+    console.log(obj[Object.keys(obj)[i]])
+}
