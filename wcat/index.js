651279a9fee2578976a31c9c641f70443fc3aae4
Himanshu Malik <jsm.28415@gmail.com>
2021-08-11 19:44:02 +0530

    split and join file text

--- wcat/index.js
+++ wcat/index.js
@@ -2,20 +2,46 @@ const fs = require("fs");
 let arguments = process.argv.slice(2);
 // console.log(arguments);
 
-let flag = [];
+let flags = [];
 let filenames = [];
 
 for(let i of arguments){
     if(i[0] == "-"){
-        flag.push(i);
+        flags.push(i);
     }
     else{
         filenames.push(i);
     }
 }
 
-if(flag.length == 0 && filenames.length != 0){
+// if(flag.length == 0 && filenames.length != 0){
+    // for(let file of filenames){
+        // console.log(fs.readFileSync(file, "utf-8"));
+    // }
+// } // to print this use this cmd - node .\index.js a.txt b.txt
+
+if(flags.length == 0){
     for(let file of filenames){
         console.log(fs.readFileSync(file, "utf-8"));
+    }
+}
+else{
+    for(let flag of flags){
+        if(flag == "-rs"){
+            for(let file of filenames){
+                let flieData = fs.readFileSync(file, "utf-8");
+                // for(let i of flieData){
+                    // console.log(i);
+                // }
+
+                // this code is to split the spaces and put in the array
+                // let fileDataArray = flieData.split(" ");
+                // console.log(fileDataArray); // To print this use this - node .\index.js -rs a.txt
+
+                // this code is to join the split spaces back without spaces
+                let fileDataArray = flieData.split(" ").join("");
+                console.log(fileDataArray);
+            }
+        }
     }
 }
